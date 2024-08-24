75bcc4e227f3cb290618695f8b130c2c76bcaaea
cwoodjr <chadmw@ovationco.com>
2024-08-23 22:49:55 -0700

    Basic UI style implemented.

--- ui/src/components/forms.py
+++ ui/src/components/forms.py
@@ -3,44 +3,77 @@ from src.types import Field, FieldType
 from src.settings import logger
 from src.api import APIClient
 from datetime import date
-from itertools import chain
+
+
+class FormField:
+    def __init__(self, field: Field, api_client: APIClient, endpoint: str):
+        self.field = field
+        self.api_client = api_client
+        self.endpoint = endpoint
+        self.is_new = False
+        self.dropdown_options = {}
+
+    def render(self):
+        """Render the form field with two columns: left for checkbox/name, right for input."""
+        left_col, right_col = st.columns([1, 2])
+        
+        with left_col:
+            if self.field.type == FieldType.FOREIGN_KEY:
+                self.is_new = st.checkbox(f'New {self.field.parent_endpoint}', key=f'{self.endpoint}-{self.field.name}-checkbox')
+            else:
+                st.write(f'*{self.field.form_name}*')  # Print the table name in italics
+
+        with right_col:
+            if self.is_new:
+                return None  # Indicates that a child form should be rendered
+            else:
+                return self._render_field_input()
+
+    def _render_field_input(self):
+        """Render the appropriate input component based on the field type."""
+        if self.field.type == FieldType.FOREIGN_KEY:
+            return self._create_foreign_key_selectbox()
+        else:
+            return BaseForm.get_field_input_component(self.field, self.endpoint)
+
+    def _create_foreign_key_selectbox(self):
+        """Create a select box for a foreign key field."""
+        if self.field.name not in self.dropdown_options:
+            self._fetch_foreign_key_options()
+
+        options = self.dropdown_options.get(self.field.name, {})
+        labels = list(options.values())
+        selected_label = st.selectbox(self._create_title(), options=labels, label_visibility='visible', key=f'{self.endpoint}-{self.field.name}')
+        selected_id = self._get_selected_foreign_key_id(options, selected_label)
+        return selected_id
+
+    def _fetch_foreign_key_options(self):
+        """Fetch options for foreign key fields."""
+        items = self.api_client.perform_crud(self.field.parent_endpoint, "GET")
+        self.dropdown_options[self.field.name] = {
+            item[self.field.parent_id]: item[self.field.parent_label] for item in items
+        }
+
+    def _get_selected_foreign_key_id(self, options, selected_label):
+        for id_, label in options.items():
+            if label == selected_label:
+                return id_
+        return None
+
+    def _create_title(self):
+        title = f"**{self.field.name.replace('_', ' ').title()}**"
+        return title + ' *' if self.field.is_required else title
 
 
 class BaseForm:
     def __init__(self, api_client: APIClient, endpoint: str, fields: list[Field]):
-        self.api_client: APIClient = api_client
-        self.endpoint: str = endpoint
-        self.dropdown_options: dict = {}
-        self.new_item_flags = {}
+        self.api_client = api_client
+        self.endpoint = endpoint
+        self.fields = [FormField(field, api_client, endpoint) for field in fields]
+        self.child_forms = {}
 
-        # Maintain a list of fields and their associated endpoints
-        self._fields: dict[str, list[Field]] = {endpoint: fields}
-
-    @property
-    def fields(self) -> dict[str, list[Field]]:
-        """Returns fields ordered by the dependency chain, if any."""
-        return self._fields
-
-    def collect_foreign_key_fields(self):
-        """Collect all foreign key fields, including from child forms."""
-        fk_fields = []
-        for endpoint, fields in self.fields.items():
-            for field in fields:
-                if field.type == FieldType.FOREIGN_KEY:
-                    fk_fields.append(field)
-                    if field.parent_allow_new:
-                        # Create a child form to check for its foreign key fields
-                        child_form_class = forms.get(field.parent_endpoint)
-                        if child_form_class:
-                            child_form_instance = child_form_class(self.api_client)
-                            fk_fields.extend(child_form_instance.collect_foreign_key_fields())
-        return fk_fields
-
-    def show_form(self, operation=None):
-        selected_operation = operation
-
-        if not operation:
-            selected_operation = st.radio("Select Operation", ["Create", "Update", "Delete"], key=self.endpoint)
+    def show_form(self, operation: str = None):
+        selected_operation = operation or st.radio("Select Operation", ["Create", "Update", "Delete"], key=self.endpoint)
 
         with st.container():
             item_id = None
@@ -49,56 +82,35 @@ class BaseForm:
 
             input_data = {}
             if selected_operation in ["Create", "Update"]:
-                # Loop over fields, ensuring that child fields are added dynamically if 'New' is selected
-                for endpoint, fields in self.fields.items():
-                    for field in fields:
-                        if field.type == FieldType.FOREIGN_KEY and selected_operation == 'Create':
-                            self.new_item_flags[field.name] = st.checkbox(f'New {field.parent_endpoint}', key=field.name)
-                            if self.new_item_flags.get(field.name):
-                                self._render_child_form(field, input_data)
-                            else:
-                                input_data[field.name] = self.get_field_input_component(field)
-                        else:
-                            input_data[field.name] = self.get_field_input_component(field)
+                self._render_form_fields(input_data)
 
             submit_button = st.button("Submit", key=f'{self.endpoint}-submit')
 
         if submit_button:
-            self._handle_submission(selected_operation, None, input_data)
+            self._handle_submission(selected_operation, item_id, input_data)
 
-    '''
-    This entire approach should change. Rather than showing a form within a form, 
-    try calculating the number of rows ahead of time, 
-    creating that many "input_components", 
-    then filling their `left_side` and `right_side` pieces with the fields 
-    and their attachment part (the name / New checkbox).
+    def _render_form_fields(self, input_data):
+        """Render all form fields, including handling of child forms."""
+        for form_field in self.fields:
+            field_data = form_field.render()
+            if form_field.is_new and field_data is None:
+                child_data = self._render_child_form(form_field.field.parent_endpoint)
+                input_data[form_field.field.name] = child_data
+            else:
+                input_data[form_field.field.name] = field_data
 
-    Set it up so that the form just rerenders once this is all ready.
+    def _render_child_form(self, endpoint: str):
+        """Render a child form and return its input data."""
+        if endpoint not in self.child_forms:
+            child_form_class = forms.get(endpoint)
+            if not child_form_class:
+                raise ValueError(f"No form class found for endpoint: {endpoint}")
+            self.child_forms[endpoint] = child_form_class(self.api_client)
 
-    Test if it is possible to use a real form, given that it'll be a different (newly generated) form each time a checkbox is clicked.
-    
-    It might simplify things to develop a class that hold both parts, the field and the checkbox. Then they dont need to be calculated on the
-    fly, can be stored in an instance of the class.
-    '''
-    def _render_child_form(self, field, input_data):
-        """Render a child form if the 'New' checkbox is selected."""
-        with st.container(border=True):
-            child_form_class = forms[field.parent_endpoint]
-            st.write(f"###### {child_form_class.name.title()}")
-            child_form_instance = child_form_class(self.api_client)
-            child_input_data = child_form_instance.show_form(operation='Create')
-            input_data[field.name] = child_input_data
-
-    def _clean_any_dates(self, data):
-        """Convert date objects to isoformat strings for serialization."""
-        cleaned_data = data.copy()
-        for key, value in cleaned_data.items():
-            if isinstance(value, date):
-                cleaned_data[key] = value.isoformat()
-        return cleaned_data
+        return self.child_forms[endpoint].show_form(operation="Create")
 
     def _handle_submission(self, operation, item_id, data):
-        """Handle form submission based on selected operation."""
+        """Handle form submission based on the selected operation."""
         serializable_data = self._clean_any_dates(data)
         if operation == "Create":
             self.api_client.perform_crud(self.endpoint, "POST", data=serializable_data)
@@ -110,70 +122,41 @@ class BaseForm:
         self.api_client.clear_cache()
         st.rerun()
 
-    def get_field_input_component(self, field: Field):
+    @staticmethod
+    def _clean_any_dates(data):
+        """Convert date objects to isoformat strings for serialization."""
+        cleaned_data = data.copy()
+        for key, value in cleaned_data.items():
+            if isinstance(value, date):
+                cleaned_data[key] = value.isoformat()
+        return cleaned_data
+
+    @staticmethod
+    def get_field_input_component(field: Field, endpoint: str):
         """Retrieve the appropriate input component for a field."""
         if field.type == FieldType.TEXT:
-            return self._create_text_input(field)
+            return st.text_input(BaseForm._create_title(field), value=field.default, key=f'{endpoint}-{field.name}')
         elif field.type == FieldType.MULTILINE_TEXT:
-            return self._create_text_area(field)
+            return st.text_area(BaseForm._create_title(field), value=field.default, key=f'{endpoint}-{field.name}')
         elif field.type == FieldType.NUMBER:
-            return self._create_number_input(field)
+            return st.number_input(BaseForm._create_title(field), value=field.default, key=f'{endpoint}-{field.name}')
         elif field.type == FieldType.DATE:
-            return self._create_date_input(field)
+            return st.date_input(BaseForm._create_title(field), value=field.default, key=f'{endpoint}-{field.name}')
         elif field.type == FieldType.BOOLEAN:
-            return self._create_checkbox(field)
-        elif field.type == FieldType.FOREIGN_KEY:
-            return self._create_foreign_key_selectbox(field)
+            return st.checkbox(BaseForm._create_title(field), value=field.default, key=f'{endpoint}-{field.name}')
         else:
             raise ValueError(f"Unsupported field type: {field.type}")
 
-    def _create_title(self, field):
+    @staticmethod
+    def _create_title(field):
         title = f"**{field.name.replace('_', ' ').title()}**"
         return title + ' *' if field.is_required else title
-
-    def _create_text_input(self, field: Field):
-        return st.text_input(self._create_title(field), value=field.default)
-
-    def _create_text_area(self, field: Field):
-        return st.text_area(self._create_title(field), value=field.default)
-
-    def _create_number_input(self, field: Field):
-        return st.number_input(self._create_title(field), value=field.default)
-
-    def _create_date_input(self, field: Field):
-        return st.date_input(self._create_title(field), value=field.default)
-
-    def _create_checkbox(self, field: Field):
-        return st.checkbox(self._create_title(field), value=field.default)
-
-    def _create_foreign_key_selectbox(self, field: Field):
-        if field.name not in self.dropdown_options:
-            self._fetch_foreign_key_options(field)
-
-        options = self.dropdown_options.get(field.name, {})
-        labels = list(options.values())
-        selected_label = st.selectbox(self._create_title(field), options=labels, label_visibility='visible')
-        selected_id = self._get_selected_foreign_key_id(options, selected_label)
-        return selected_id
-
-    def _fetch_foreign_key_options(self, field: Field):
-        """Fetch options for foreign key fields."""
-        items = self.api_client.perform_crud(field.parent_endpoint, "GET")
-        self.dropdown_options[field.name] = {
-            item[field.parent_id]: item[field.parent_label] for item in items
-        }
-
-    def _get_selected_foreign_key_id(self, options, selected_label):
-        for id_, label in options.items():
-            if label == selected_label:
-                return id_
-        return None
 
 
 # Define specific forms for each endpoint
 class ResumeForm(BaseForm):
     name = 'resumes'
-    fields_list: list[Field] = [
+    fields_list = [
         Field(name='data', type=FieldType.TEXT, is_required=True, form_name=name)
     ]
 
@@ -183,7 +166,7 @@ class ResumeForm(BaseForm):
 
 class PostingForm(BaseForm):
     name = 'postings'
-    fields_list: list[Field] = [
+    fields_list = [
         Field(name='platform', type=FieldType.TEXT, is_required=True, form_name=name),
         Field(name='company', type=FieldType.TEXT, is_required=True, form_name=name),
         Field(name='title', type=FieldType.TEXT, is_required=True, form_name=name),
@@ -200,7 +183,7 @@ class PostingForm(BaseForm):
 
 class ApplicationForm(BaseForm):
     name = 'applications'
-    fields_list: list[Field] = [
+    fields_list = [
         Field(name='posting_id', type=FieldType.FOREIGN_KEY, is_required=True, parent_endpoint='postings', parent_id='id', parent_label='title', parent_allow_new=True, form_name=name),
         Field(name='resume_id', type=FieldType.FOREIGN_KEY, is_required=True, parent_endpoint='resumes', parent_id='id', parent_label='data', parent_allow_new=True, form_name=name),
         Field(name='date_submitted', type=FieldType.DATE, is_required=True, form_name=name),
@@ -212,7 +195,7 @@ class ApplicationForm(BaseForm):
 
 class ResponseTypeForm(BaseForm):
     name = 'response_types'
-    fields_list: list[Field] = [
+    fields_list = [
         Field(name='name', type=FieldType.TEXT, is_required=True, form_name=name)
     ]
 
@@ -222,7 +205,7 @@ class ResponseTypeForm(BaseForm):
 
 class ResponseForm(BaseForm):
     name = 'responses'
-    fields_list: list[Field] = [
+    fields_list = [
         Field(name='application_id', type=FieldType.FOREIGN_KEY, is_required=True, parent_endpoint='applications', parent_id='id', parent_label='id', parent_allow_new=True, form_name=name),
         Field(name='response_type_id', type=FieldType.FOREIGN_KEY, is_required=True, parent_endpoint='response_types', parent_id='id', parent_label='name', parent_allow_new=True, form_name=name),
         Field(name='date_received', type=FieldType.DATE, is_required=True, form_name=name),
