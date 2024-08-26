bb65a08356f11e8afbd55836a5906fcb9eacf7af
cwoodjr <chadmw@ovationco.com>
2024-08-26 13:38:58 -0700

    Fixed submissions occur in wrong order

--- ui/src/components/forms.py
+++ ui/src/components/forms.py
@@ -18,28 +18,64 @@ class FormRow:
 
     In a future update, the FormRow would ideally have a fixed height.
     '''
-    def __init__(self, field: Field, api_client: APIClient):
+    def __init__(self, field: Field, api_client: APIClient, parent=None):
         self.field = field
         self.api_client = api_client
         self.is_new = False
         self.dropdown_options = {}
+        self.pending_id_from = None  # Track if this field is pending a foreign key
 
-    def render(self):
-        """Render the form field with two columns: left for checkbox/name, right for input."""
+    def render(self, fk_tracker: dict = None):
+        """Render the form row with two columns."""
+        fk_tracker = fk_tracker or {}
         left_col, right_col = st.columns([1, 3])
-        
+
         with left_col:
-            if self.field.type == FieldType.FOREIGN_KEY:
-                self.is_new = st.checkbox(f'New', key=f'{self.field.name}-checkbox')
-            else:
-                st.write(f'*{self.field.form_name}*')  # Print the table name in italics
+            self._render_left_column(fk_tracker)
 
         with right_col:
-            if self.is_new:
-                st.write(f'###### *Creating new {self.field.parent_endpoint}*')
-                return None  # Indicates that a new entity is being created
-            else:
-                return self._render_field_input()
+            return self._render_right_column()
+
+    def _render_left_column(self, fk_tracker):
+        """Render the left column, handling the 'New' checkbox or displaying the field's name."""
+        if self.field.type == FieldType.FOREIGN_KEY:
+            self._handle_foreign_key_checkbox(fk_tracker)
+        else:
+            self._display_field_name()
+
+    def _handle_foreign_key_checkbox(self, fk_tracker):
+        """Handle the logic for the 'New' checkbox in foreign key fields."""
+        self._initialize_foreign_key_tracker(fk_tracker)
+        self.is_new = st.checkbox('New', key=f'{self.field.name}-checkbox')
+        self._update_foreign_key_tracking(fk_tracker)
+
+    def _initialize_foreign_key_tracker(self, fk_tracker):
+        """Ensure that the foreign key tracker is initialized for this field's endpoint."""
+        if not fk_tracker.get(self.field.form_endpoint):
+            logger.info(f'Init tracking for {self.field.form_endpoint}')
+            fk_tracker[self.field.form_endpoint] = set()
+
+    def _update_foreign_key_tracking(self, fk_tracker):
+        """Update the tracking of foreign key dependencies based on the 'New' checkbox."""
+        endpoint_tracker = fk_tracker[self.field.form_endpoint]
+        if self.is_new:
+            self.pending_id_from = self.field.parent_endpoint
+            endpoint_tracker[self.field.parent_endpoint] = self.field.name
+            logger.info(f'FK tracker updated: {endpoint_tracker}')
+        else:
+            endpoint_tracker.pop(self.field.parent_endpoint, None)
+            self.pending_id_from = None
+
+    def _display_field_name(self):
+        """Display the field's name in italic in the left column."""
+        st.write(f'*{self.field.form_name}*')
+
+    def _render_right_column(self):
+        """Render the right column, displaying either the input field or a placeholder."""
+        if self.is_new:
+            st.write(f'###### *Creating new {self.field.parent_endpoint}*')
+            return None
+        return self._render_field_input()
 
     def _render_field_input(self):
         """Render the input component based on the field type."""
@@ -49,7 +85,7 @@ class FormRow:
             return DynamicForm.get_field_input_component(self.field)
 
     def _create_foreign_key_selectbox(self):
-        """Create a select box for a foreign key field."""
+        """Create a selectbox; click behavior propogates additional fields."""
         if self.field.name not in self.dropdown_options:
             self._fetch_foreign_key_options()
 
@@ -60,7 +96,6 @@ class FormRow:
         return selected_id
 
     def _fetch_foreign_key_options(self):
-        """Fetch options for foreign key fields."""
         items = self.api_client.perform_crud(self.field.parent_endpoint, "GET")
         self.dropdown_options[self.field.name] = {
             item[self.field.parent_id]: item[self.field.parent_label] for item in items
@@ -83,9 +118,8 @@ class DynamicForm:
     fields when a user selects the "New" checkbox for a foreign-key field. All fields are rendered in a single form with
     a unified submit button at the bottom.
 
-    This form does not preserve the order of fields. When new fields are added due to a foreign-key relationship, they are
-    inserted at the end of the form. In a future update, it is prefered that new fields join immediately after the triggering 
-    foreign-key field, pushing subsequent fields down in the form.
+    When new fields are added due to a foreign-key relationship, they are inserted immediately after the foreign key field.
+    This is preservation of intuitive field order.
 
     There is some instability with FormRow positions while adding new fields. FieldRows whose position isn't changing, are
     bouncing around. This would ideally be fixed in a future update.
@@ -94,19 +128,31 @@ class DynamicForm:
     are processed in the correct order according to their dependencies.
     '''
     def __init__(self, api_client: APIClient, fields: list[Field]):
-        self.api_client = api_client # Used for data retrieval, employs caching
-        self.endpoints_data = {} # Maps form endpoints to their input data
-        self.rows = [] # Cache of all rows in form. Recalcd when rows change.
+        self.api_client: APIClient = api_client # Used for data retrieval, employs caching
+        self.input_data: dict[str, dict[str, object]] = {} # Maps form endpoints to each fields input
+        self.rows: list[Field] = [] # Cache of all rows in form. Recalcd when rows change.
+        self.pending_fk: dict[str, dict[str, str]] = {}  # Tracks foreign keys that need to be resolved
         self._initialize_rows(fields)
 
     def _initialize_rows(self, fields, insert_after=None):
         """Initialize the fields and structure for the nested data."""
         new_rows = []
         for field in fields:
-            if field.form_name not in self.endpoints_data:
-                self.endpoints_data[field.form_endpoint] = {}
-            new_rows.append(FormRow(field, self.api_client))
+            if field.form_endpoint not in self.pending_fk:
+                self.pending_fk[field.form_endpoint] = {}
 
+            # Track foreign key fields that may need resolution
+            if field.type == FieldType.FOREIGN_KEY:
+                self.pending_fk[field.form_endpoint][field.parent_endpoint] = None
+
+            # Track input data
+            if field.form_name not in self.input_data:
+                self.input_data[field.form_endpoint] = {}
+            
+            row = FormRow(field, self.api_client)
+            new_rows.append(row)
+
+        # Preserves field order by inserting at location
         if insert_after == None:
             self.rows.extend(new_rows)
         else:
@@ -124,13 +170,13 @@ class DynamicForm:
 
     def _render_form_rows(self):
         """Render all form fields, dynamically including related fields."""
-        for i, form_row in enumerate(self.rows):
-            field_data = form_row.render()
-            if form_row.is_new and field_data is None:
+        for i, row in enumerate(self.rows):
+            field_data = row.render(fk_tracker=self.pending_fk)
+            if row.is_new and field_data is None:
                 # Adds fields to self.rows, after current iteration
-                self._add_child_rows(form_row.field.parent_endpoint, insert_after=i)
+                self._add_child_rows(row.field.parent_endpoint, insert_after=i)
             else:
-                self.endpoints_data[form_row.field.form_endpoint][form_row.field.name] = field_data
+                self.input_data[row.field.form_endpoint][row.field.name] = field_data
 
     def _add_child_rows(self, parent_endpoint, insert_after):
         """Add fields from a child endpoint dynamically to the form."""
@@ -141,18 +187,45 @@ class DynamicForm:
         child_form = child_form_class(self.api_client)
         self._initialize_rows(child_form.fields_list, insert_after=insert_after)
 
+    def _supply_any_pending_ids(self, endpoint, data, foreign_keys):
+        """Inject any necessary foreign key values into the submission data."""
+        pending_for_endpoint = self.pending_fk[endpoint]
+        logger.info(f'Endpoint {endpoint} requires: {pending_for_endpoint}')
+        
+        for parent_endpoint, pending_fk in pending_for_endpoint.items():
+            parent_id = foreign_keys.get(parent_endpoint)
+            if parent_id is not None:
+                data[pending_fk] = parent_id
+                logger.info(f'Injected {parent_id} into {pending_fk} for {endpoint}.')
+            else:
+                raise KeyError(f'Missing foreign key for {pending_fk} in {endpoint}. Foreign_keys: {pending_for_endpoint}')
+
+        return data
+
     def _handle_submission(self):
         """Handle form submission."""
-        serializable_data = self._clean_any_dates(self.endpoints_data)
+        serializable_data = self._clean_any_dates(self.input_data)
+        logger.info(f'Serializable data before submission: {serializable_data}')
         
+        foreign_keys = {}
         # Submit all collected data in the correct order
-        for endpoint, data in serializable_data.items():
-            if data:
-                logger.info(f'submitting: {data}')
-                self.api_client.perform_crud(endpoint, "POST", data=data)
+        for endpoint, raw_data in reversed(serializable_data.items()):
+            if raw_data:
+                ready_data = self._supply_any_pending_ids(endpoint, raw_data, foreign_keys)
+                logger.info(f'Sending {endpoint} data: {ready_data}')
+                response = self.api_client.perform_crud(endpoint, "POST", data=ready_data)
+                foreign_keys[endpoint] = response.get('id')
 
+        logger.info(f'Current FKs: {foreign_keys}')
         self.api_client.clear_cache()
         st.rerun()
+
+    def _get_row(self, endpoint, field_name):
+        """Retrieve a Field object by its name."""
+        for row in self.rows:
+            if row.field.form_endpoint == endpoint and row.field.name == field_name:
+                return row
+        return None
 
     @staticmethod
     def _clean_any_dates(data):
